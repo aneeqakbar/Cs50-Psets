548ca02bd04e9940bd17458769270818f1b8f44d
skipastyyyy <skipastyyyy@users.noreply.github.com>
2020-12-13 17:02:15 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -24,11 +24,11 @@ int main(void){
     // printf("%f\n",average(letters,words));
     // printf("%f\n",average(sentences,words));
     if (round(Grade) >= 16){
-        printf("Grade: 16+\n");
+        printf("Grade 16+\n");
     }else if (round(Grade) < 1){
         printf("Before Grade 1\n");
     }else{
-        printf("Grade: %.lf\n",round(Grade));
+        printf("Grade %.lf\n",round(Grade));
     }
 }
 
